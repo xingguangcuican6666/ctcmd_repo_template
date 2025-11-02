1c2d88969c5b9c3ee87b65b5ca6a36b32f307eec
xingguangcuican6666 <xingguangcuican666@foxmail.com>
2025-11-02 14:55:45 +0800

    upload

---
+++ config.bat
@@ -0,0 +1,4 @@
+set GIT_SSH_COMMAND=ssh -i "C:\\Users\\xingguangcuican\\.ssh\\id_rsa"
+set git_username=xingguangcuican6666
+set git_email=xingguangcuican666@foxmail.com
+set repo=git@github.com:xingguangcuican6666/ctcmd_repo_template.git
